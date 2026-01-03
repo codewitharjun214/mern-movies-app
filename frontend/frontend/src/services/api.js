09fefbf282a6fc3417f28ce10e93f38694e667d6
Arjun Kadam <153195543+codewitharjun214@users.noreply.github.com>
2026-01-03 11:34:39 +0530

    Update frontend API URL to Render backend

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://mern-movies-app-x7wa.onrender.com/api"
+
 });
 
 export const getAllMovies = () => API.get("/movies");
